8d5d0a1096545a0dda87937bac17156f885cb877
Vadim Frolov <stefaniafrolovafrolov@yandex.ru>
2023-10-11 14:43:54 +0300

    webpack lesson 1 nodejs 16

--- src/Header.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-
-export function Header() {
-  return (
-    <header>
-      <h1>Добро пожаловать в приложение React, тестим webpack!</h1>
-    </header>
-  )
-}
--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react"
-import * as RaectDom from "react-dom"
-import { Header } from "./Header"
-
-
-window.addEventListener("load", () => {
-  RaectDom.render(<Header />, document.getElementById("react_root"))
-})
